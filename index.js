969b3ad15ed6f3b487858f2588a8bbdfc5d4bab4
Pedro Elias <106087744+Elias-Pdr@users.noreply.github.com>
2022-09-22 16:41:33 -0300

    Up

---
+++ index.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+import { createTransport } from 'nodemailer';
+
+const transportador = createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.SENHA
+    }
+});
+
+const emailASerEnviado = {
+    from: process.env.EMAIL,
+    to: 'purplefire0008@proton.me',
+    subject: 'Muita luta 4',
+    text: 'ue?'
+}
+
+transportador.sendMail(emailASerEnviado, (err) => {
+    if (err){
+        console.log(err);
+        return
+    }
+    console.log('Email enviado!');
+
+})
+console.log('...');
